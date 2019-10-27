0cc3f601a705614741a4ab10db7cc389439fd8dc
erictong38 <36332269+erictong38@users.noreply.github.com>
2019-10-27 12:00:59 -0700

    adding sql

---
+++ www/scripts/databaseHandler.js
@@ -0,0 +1,29 @@
+﻿var databaseHandler = {
+    db: null,
+    createDatabase: function () {
+        this.db = window.openDatabase(
+            "users.db",
+            "1.0",
+            "users database",
+            1000000);
+        this.db.transaction(
+            function (tx) {
+                //Run SQL Here
+                tx.executeSql(
+                    "create table if not exists users(_id int primary key, username, password)",
+                [],
+                    function (tx, results) { },
+                    function (tx, error) {
+                        console.log("Error while creating the table: " + error.message);
+                    }
+                );
+            },
+            function (error) {
+                console.log("Transaction error:" + error.message);
+            },
+            function () {
+                console.log("Create DB transaction completed successfully:");
+            },
+        );
+    }
+}
---
+++ www/scripts/usersHandler.js
@@ -0,0 +1,19 @@
+﻿var usersHandler = {
+    //Add the record in database, it adds record or row in Web SQL (SQLite)
+    addUser: function (username, password) {
+        databaseHandler.db.transaction(
+            function (tx) {
+                tx.executeSql(
+                    "insert into users(username, password) values(?, ?)",
+                    [username, password],
+                    function (tx, results) { },
+                    function (tx, error) {
+                        console.log("add user error: " + error.message);
+                    }
+                );
+            },
+            function (error) { },
+            function () { }
+        );
+    },
+}
